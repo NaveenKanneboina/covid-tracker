91ae81d8944775a825cbb2c809f297c84828f5fb
Naveen Kumar Kanneboina <naveenkanneboian1303@gmail.com>
2020-08-19 21:56:18 +0530

    general changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
-import React from 'react';
+import React,{useState, useEffect} from 'react';
 import './App.css';
-
+import {FormControl , Select, MenuItem} from '@material-ui/core';
 function App() {
+  const [countries, setcountries] = useState(["hello","world","hey"]);
+
+useEffect(() => {
+  
+}, [])
+
+
   return (
-    <div className="App">
-      <h1> Hello World</h1>
+    <div className="app">
+      <div className="header">
+        <h1>Covid Tracker</h1>
+        <h1>https://disease.sh/v3/covid-19/countries</h1>
+        <FormControl>
+          <Select variant="outlined" value="WorldWide">
+            <MenuItem value="wolrdWide">WorldWide</MenuItem>
+            {
+              countries.map((country)=>(
+                <MenuItem value={country}>{country}</MenuItem>
+                ))
+            }
+            
+          </Select>
+        </FormControl>
+      </div>
     </div>
   );
 }
